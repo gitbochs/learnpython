f065f5046168fd4113af2dc20dff298904ff1f10
Guogle <guoshengzhao2008@gmail.com>
2016-11-29 16:50:46 +0800

    使用chrome访问url

--- vote.py
+++ vote.py
@@ -1,6 +1,17 @@
 #coding:utf-8
 
 from selenium import webdriver
+import time
 
 dr = webdriver.Chrome()
+time.sleep(2)
+print ("最大化chrome,访问url")
 
+url = 'http://www.baidu.com'
+dr.maximize_window()
+dr.get(url)
+
+time.sleep(2)
+print ("关闭chrome")
+
+#dr.close()
